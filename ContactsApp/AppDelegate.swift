cbb60f965e898043c020922e12d6254c2de135e4
Nikita <a2143319@yandex.ru>
2020-08-14 19:22:51 +0500

    Contacts

--- ContactsApp/AppDelegate.swift
+++ ContactsApp/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-//test
+//app
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
