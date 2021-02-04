debde40ba2bc7e8d011e7cf6c6e2da57336f0dee
Nikita Dmitriev <a2143319@yandex.ru>
2021-02-04 17:39:05 +0500

    New

--- ContactsApp/TableViewController.swift
+++ ContactsApp/TableViewController.swift
@@ -9,13 +9,11 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
     
-    
-   private let searchController = UISearchController(searchResultsController: nil)
+    private let searchController = UISearchController(searchResultsController: nil)
     
     private var filteredContacts = [Contact]()
-    
+    //func updateSearchResults
     private var searchBarIsEmpty: Bool {
         guard let text = searchController.searchBar.text else { return false }
         return text.isEmpty
@@ -41,6 +39,7 @@ class TableViewController: UITableViewController {
         loadContacts {
             DispatchQueue.main.async {
                 self.tableView.reloadData()
+                
             }
         }
         
@@ -51,14 +50,13 @@ class TableViewController: UITableViewController {
         navigationItem.searchController = searchController
         definesPresentationContext = true
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         if isFiltering {
@@ -66,11 +64,11 @@ class TableViewController: UITableViewController {
         }
         return contacts.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         var contact: Contact
         if isFiltering {
             contact = filteredContacts[indexPath.row] //получаем контакт для ячейки
@@ -85,7 +83,7 @@ class TableViewController: UITableViewController {
         
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { // когда нажали на ячейку
         performSegue(withIdentifier: "goToOneContact", sender: self) // переход по сигвею и нажатию
     }
@@ -109,50 +107,9 @@ class TableViewController: UITableViewController {
         }
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    
-    */
-
 }
+
+// ищем контакт по имени или телефону
 
 extension TableViewController: UISearchResultsUpdating {
     
@@ -163,7 +120,10 @@ extension TableViewController: UISearchResultsUpdating {
     private func filterContentForSearchName(_ searchText: String) {
         
         filteredContacts = contacts.filter({ (contact: Contact) -> Bool in
-            return contact.name.lowercased().contains(searchText.lowercased())
+            guard contact.name.lowercased().contains(searchText.lowercased()) || contact.phone.contains(searchText) else {
+                return false
+            }
+            return true
         })
         tableView.reloadData()
     }
